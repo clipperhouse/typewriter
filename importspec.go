c167fc926a4dbee6bac2c2e1f8d1bba9d91ff511
Matt Sherman <mwsherman@gmail.com>
2014-11-22 22:49:13 -0500

    gen ImportSpec slice

--- importspec.go
+++ importspec.go
@@ -2,6 +2,8 @@ package typewriter
 
 // ImportSpec describes the name and path of an import.
 // The name is often omitted.
+//
+// +gen slice:"Select[string]"
 type ImportSpec struct {
 	Name, Path string
 }
