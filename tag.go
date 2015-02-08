ed39812e7e4f7e8cecc1cc48d4c63e7cee6af06f
Matt Sherman <mwsherman@gmail.com>
2015-02-08 18:17:23 -0500

    lose special strings for tags

--- tag.go
+++ tag.go
@@ -1,7 +1,5 @@
 package typewriter
 
-import "strings"
-
 // +gen slice
 type Tag struct {
 	Name    string
@@ -9,24 +7,8 @@ type Tag struct {
 	Negated bool
 }
 
-func (t Tag) String() string {
-	return t.Name
-}
-
 type TagValue struct {
-	Name           string
-	TypeParameters []Type
-}
-
-func (v TagValue) String() string {
-	if len(v.TypeParameters) == 0 {
-		return v.Name
-	}
-
-	var a []string
-	for i := 0; i < len(v.TypeParameters); i++ {
-		a = append(a, v.TypeParameters[i].Name)
-	}
-
-	return v.Name + "[" + strings.Join(a, ",") + "]"
+	Name               string
+	TypeParameters     []Type
+	typeParameterNames []string
 }
